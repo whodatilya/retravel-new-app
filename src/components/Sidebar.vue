6f4d917cd672a0882da08a9c912c59946f801947
Ilya.Gordeev <gordeevilyas@yandex.ru>
2023-11-16 17:18:31 +0300

    Almost done sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,13 +1,24 @@
 <template>
-  <div class="flex mt-[69px] ml-[52px]">
+  <div class="flex pt-16 pl-14">
     <div class="flex flex-col gap-4">
-      <img :src="logo" alt="Логотип">
+      <img :src="logo" alt="Логотип" class="mb-4">
       <div
-        class="menu__item flex p-4 gap-4 br-10 items-center font-semibold"
+        class="flex gap-6 br-10 menu__text"
+        :class="{ 'menu__text-active': selectedPage === 'Settings' }"
+        @click="switchPage('Settings')"
+      >
+        <img :src="iconUser" alt="Пользователь">
+        <div class="flex flex-col justify-center user-text">
+          <div class="menu__text fs-18 font-semibold">Имя Фамилия</div>
+          <div class="menu__text fs-14 font-medium">Пользователь</div>
+        </div>
+      </div>
+      <div
+        class="menu__text fs-18 menu__item p-4 gap-4 br-10 font-semibold"
         v-for="(item, index) in menuItems"
         :key="index"
-        @click="switchPage(item)"
-        :class="{ 'menu__item-active': item.componentName === selectedPage }"
+        @click="switchPage(item.componentName)"
+        :class="{ 'menu__text-active': item.componentName === selectedPage }"
       >
         <img
           class="menu__item-image"
@@ -24,6 +35,7 @@
 import { defineComponent } from "vue";
 import logo from "@/assets/images/logo.svg";
 import iconChat from '@/assets/images/iconChat.svg'
+import iconUser from '@/assets/images/iconUser.svg'
 import iconFavourite from '@/assets/images/iconFavourite.svg'
 import iconFolder from '@/assets/images/iconFolder.svg'
 import iconHome from '@/assets/images/iconHome.svg'
@@ -34,6 +46,7 @@ export default defineComponent({
   data () {
     return {
       logo,
+      iconUser,
       menuItems: [
         {
           title: 'Главная',
@@ -58,15 +71,15 @@ export default defineComponent({
         {
           title: 'Избранное',
           icon: 'iconFavourite',
-          componentName: 'Favourite'
+          componentName: 'Favourites'
         }
       ],
       selectedPage: 'Main'
     }
   },
   methods: {
-    switchPage(item) {
-      this.selectedPage = item.componentName
+    switchPage(componentName) {
+      this.selectedPage = componentName
     },
     processQuit() {
       this.isOpen = !this.isOpen
@@ -79,16 +92,29 @@ export default defineComponent({
 </script>
 <style scoped lang="sass">
 $main-green: #4E944F
-.menu__item
-  font-family: 'Lato', system-ui
-  font-style: normal
-  color: #7D7D7D
-  letter-spacing: 0.01em
-  font-size: 18px
-  line-height: 22px
-  &:hover
-    cursor: pointer
-  &-active
-    color: #FFFFFF
-    background: $main-green
+.menu
+  &__item
+    display: flex
+    align-items: center
+    &:hover
+      cursor: pointer
+    &-image
+      &-active
+        filter: brightness(0) invert(1)
+  &__text
+    font-family: 'Lato', system-ui
+    font-style: normal
+    color: #7D7D7D
+    letter-spacing: 0.01em
+    line-height: normal
+    &-active
+      color: #FFFFFF
+      background: $main-green
+      .menu__text
+        color: #FFFFFF
+.fs
+  &-18
+    font-size: 18px
+  &-14
+    font-size: 14px
 </style>
